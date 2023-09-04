a3a26bed0380c16fe78e1b23975d88b728ca795c
sec0ndw1nd <phj98521@naver.com>
2023-09-04 21:06:35 +0900

    feat(tic-tac-toe): add a toggle button that lets you sort the moves in either ascending or descending order.

--- tic-tac-toe/src/components/Game.js
+++ tic-tac-toe/src/components/Game.js
@@ -4,6 +4,7 @@ import Board from './Board';
 function Game() {
   const [history, setHistory] = useState([Array(9).fill(null)]);
   const [turn, setTurn] = useState(0);
+  const [isDescending, setIsDescending] = useState(false);
   const currentSquares = history[turn];
   const currentPlayer = turn % 2 ? 'X' : 'O';
 
@@ -24,14 +25,20 @@ function Game() {
       </div>
       <div className="game-info">
         <div className="status">{`Current Turn: ${turn}`}</div>
+        <button onClick={() => setIsDescending((prev) => !prev)}>
+          order by {isDescending ? 'ascending' : 'descending'}
+        </button>
         <ol>
-          {history.map((_, n) => (
-            <li key={`history-list-` + n}>
-              <button onClick={() => handleHistory(n)}>
-                {n > 0 ? `Go to turn #${n}` : `Go to game start`}
-              </button>
-            </li>
-          ))}
+          {history.map((_, n) => {
+            const orderNum = isDescending ? history.length - 1 - n : n;
+            return (
+              <li key={`history-list-` + orderNum}>
+                <button onClick={() => handleHistory(orderNum)}>
+                  {orderNum > 0 ? `Go to turn #${orderNum}` : `Go to game start`}
+                </button>
+              </li>
+            );
+          })}
         </ol>
       </div>
     </div>
