6cb254983bd2a3f9c16459c032e0f1551f54e737
sec0ndw1nd <phj98521@naver.com>
2023-09-06 17:49:05 +0900

    initial commit - ugly-searchbar

---
+++ ugly-searchbar/src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div className="App">
+        <header className="App-header">
+          <p>This is my own react boilerplate for some exercises.</p>
+          <a
+            className="App-link"
+            href="https://react.dev/reference/react"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            React Docs
+          </a>
+        </header>
+      </div>
+    </>
+  );
+}
+
+export default App;
