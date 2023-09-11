43f6e89e9e8f721d1f67890ccbe4da7b8b7c61f2
sec0ndw1nd <phj98521@naver.com>
2023-09-11 20:11:25 +0900

    feat(learn-react-hooks): add components to usingRef.js for learning useRef

--- learn-react-hooks/src/pages/part1/UsingRef.js
+++ learn-react-hooks/src/pages/part1/UsingRef.js
@@ -1,5 +1,76 @@
-function UsingRef() {
-  return <>useRef</>;
+import { useRef, useState } from 'react';
+
+function Counter() {
+  const countRef = useRef(0);
+  const onClickCount = () => {
+    countRef.current++;
+    alert(`You clicked ${countRef.current} times!`);
+  };
+
+  return (
+    <div>
+      <h1>Counter</h1>
+      <span>This count will not update because ref does not trigger a re-render.</span>
+      <div>count: {countRef.current}</div>
+      <button onClick={onClickCount}>add count</button>
+    </div>
+  );
 }
 
-export default UsingRef;
+function Stopwatch() {
+  const [startTime, setStartTime] = useState(null);
+  const [currentTime, setCurrentTime] = useState(null);
+  const timeoutRef = useRef(null);
+
+  const handleStart = () => {
+    setStartTime(Date.now());
+    setCurrentTime(Date.now());
+    console.log('hi');
+
+    clearInterval(timeoutRef.current);
+    timeoutRef.current = setInterval(() => {
+      setCurrentTime(Date.now());
+    }, 30);
+  };
+  const handleStop = () => {
+    clearInterval(timeoutRef.current);
+  };
+
+  let secondsPassed = 0;
+  if (startTime != null && currentTime != null) {
+    secondsPassed = (currentTime - startTime) / 1000;
+  }
+
+  return (
+    <div>
+      <h1>Stopwatch</h1>
+      <div>Time passed: {secondsPassed.toFixed(3)}</div>
+      <button onClick={handleStart}>Start</button>
+      <button onClick={handleStop}>Stop</button>
+    </div>
+  );
+}
+
+function FocusMe() {
+  const inputRef = useRef(null);
+
+  const handlerFocusInput = () => inputRef.current.focus();
+
+  return (
+    <div>
+      <h1>Say</h1>
+      <input ref={inputRef} />
+      <button onClick={handlerFocusInput}>Focus Input</button>
+    </div>
+  );
+}
+
+export default function UsingRef() {
+  return (
+    <>
+      <Counter />
+      <Stopwatch />
+      <FocusMe />
+    </>
+  );
+}
