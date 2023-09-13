cb4cef8c7b40499d158a6b4d8fa2b1b2e1a7851d
sec0ndw1nd <phj98521@naver.com>
2023-09-13 22:24:36 +0900

    feat(learn-react-hooks): add components to UsingCallback.js for learning useCallback

--- learn-react-hooks/src/pages/part1/UsingCallback.js
+++ learn-react-hooks/src/pages/part1/UsingCallback.js
@@ -1,5 +1,31 @@
+import { useCallback, useEffect, useState } from 'react';
+
 function UsingCallback() {
-  return <>useCallback</>;
+  const [age, setAge] = useState(0);
+  const [isChecked, setIsChecked] = useState(false);
+
+  const handler = useCallback(() => console.log(`your age is ${age}`), [age]);
+
+  useEffect(() => {
+    console.log('handler updated.');
+  }, [handler]);
+
+  return (
+    <>
+      <div>
+        <span>your age: </span>
+        <input type="number" value={age} onChange={(e) => setAge(e.target.value)} />
+        <button onClick={handler}>Confirm</button>
+      </div>
+      <input
+        id="checkbox"
+        type="checkbox"
+        value={isChecked}
+        onChange={() => setIsChecked(!isChecked)}
+      />
+      <label htmlFor="checkbox">checkbox</label>
+    </>
+  );
 }
 
 export default UsingCallback;
