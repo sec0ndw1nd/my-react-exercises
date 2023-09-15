192542d4d93007b8fb96de922b8350ed23b553cb
sec0ndw1nd <phj98521@naver.com>
2023-09-15 21:48:59 +0900

    feat(learn-react-hooks): add some components to UsingEffect.js

--- learn-react-hooks/src/pages/part1/UsingEffect.js
+++ learn-react-hooks/src/pages/part1/UsingEffect.js
@@ -1,5 +1,43 @@
+import { useState, useEffect } from 'react';
+
+const boxStyle = {
+  margin: '50px 0',
+};
+const spanStyle = {
+  marginLeft: '10px',
+};
+
+function SpamConsole() {
+  console.log('spamconsole rendered');
+
+  useEffect(() => {
+    console.log('useEffect called');
+    const interval = setInterval(() => console.log('SpamConsole Mounted!!!'), 1000);
+    return () => {
+      clearInterval(interval);
+      console.log('SpamConsole killed');
+    };
+  }, []);
+
+  return <span style={spanStyle}>Spamming console.log now.</span>;
+}
+
 function UsingEffect() {
-  return <>useEffect</>;
+  const [isMounted, setIsMounted] = useState(false);
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div style={boxStyle}>
+        <button onClick={() => setIsMounted(!isMounted)}>toggle span</button>
+        {isMounted && <SpamConsole />}
+      </div>
+      <div style={boxStyle}>
+        <button onClick={() => setCount((prev) => ++prev)}>add count</button>
+        <span style={spanStyle}>count: {count}</span>
+      </div>
+    </>
+  );
 }
 
 export default UsingEffect;
