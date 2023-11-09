af7d84010b8e068898af68fffcdda9307887f3e6
sec0ndw1nd <phj98521@naver.com>
2023-11-09 20:29:32 +0900

    refactor(section21-advanced-router): use common action(manipulateEventAction) on EditEventPage and NewEventPage

--- section21-advanced-router/frontend/src/components/EventForm.js
+++ section21-advanced-router/frontend/src/components/EventForm.js
@@ -1,5 +1,7 @@
 import {
   Form,
+  json,
+  redirect,
   useActionData,
   useNavigate,
   useNavigation,
@@ -19,7 +21,7 @@ function EventForm({ method, event }) {
   }
 
   return (
-    <Form method="POST" className={classes.form}>
+    <Form method={method} className={classes.form}>
       {actionData && actionData.errors && (
         <ul>
           {Object.values(actionData.errors).map((err) => (
@@ -80,3 +82,40 @@ function EventForm({ method, event }) {
 }
 
 export default EventForm;
+
+export async function action({ request, params }) {
+  const method = request.method;
+
+  const data = await request.formData();
+
+  const eventData = {
+    title: data.get('title'),
+    image: data.get('image'),
+    date: data.get('date'),
+    description: data.get('description'),
+  };
+
+  let url = 'http://localhost:8080/events';
+  if (method.toUpperCase() === 'PATCH') {
+    url += `/${params.eventId}`;
+  }
+
+  const response = await fetch(url, {
+    method,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(eventData),
+  });
+
+  // invalid inputs
+  if (response.status === 422) {
+    return response;
+  }
+
+  if (!response.ok) {
+    throw json({ message: 'Could not save event.' }, { status: 500 });
+  }
+
+  return redirect('/events');
+}
--- section21-advanced-router/frontend/src/pages/EditEventPage.js
+++ section21-advanced-router/frontend/src/pages/EditEventPage.js
@@ -1,10 +1,10 @@
-import { useLoaderData, useParams, useRouteLoaderData } from 'react-router-dom';
+import { useRouteLoaderData } from 'react-router-dom';
 import EventForm from '../components/EventForm';
 
 function EditEventPage() {
   const data = useRouteLoaderData('event-detail');
 
-  return <EventForm event={data.event} />;
+  return <EventForm method="patch" event={data.event} />;
 }
 
 export default EditEventPage;
